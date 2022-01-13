0501d73e08672dff33dad4003f122f7a2097a886
Panagiotis Ioannidis <ioannidis_panos@live.se>
2022-01-13 13:35:38 +0100

    first commit

---
+++ src/main/kotlin/Tomteland.kt
@@ -0,0 +1,39 @@
+
+class Tomteland{
+
+    var hashMap : HashMap<String, List<String>> = HashMap()
+    init {
+        hashMap.put("Tomten", listOf( "Glader", "Butter",
+            "Tröger", "Trötter", "Blyger", "Rådjuret", "Nyckelpigan", "Haren", "Räven",
+            "Skumtomten", "Gråsuggan", "Butter", "Myran",
+            "Dammråttan", "Bladlusen"))
+        hashMap.put("Glader", listOf("Tröger", "Trötter", "Blyger", "Dammråttan", "Skumtomten"))
+        hashMap.put("Butter", listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven", "Gråsuggan", "Myran", "Bladlusen"))
+        hashMap.put("Trötter", listOf("Dammråttan", "Skumtomten"))
+        hashMap.put("Skumtomten", listOf("Dammråttan"))
+        hashMap.put("Räven", listOf("Gråsuggan", "Myran", "Bladlusen"))
+        hashMap.put("Myran", listOf("Bladlusen"))
+    }
+
+    fun getUnderlings(currentName: String, res: MutableList<String>): List<String> {
+        hashMap.get(currentName)?.forEach {
+            res.add(it)
+            if(hashMap.contains(it)) {
+                hashMap.get(it)!!.forEach {
+                    res += getUnderlings(it, res)
+                }
+            }
+        }
+        return res.distinct()
+    }
+}
+
+fun main() {
+    val tomteland = Tomteland()
+
+    val list: MutableList<String> = mutableListOf()
+    val name: String = readLine()!!.toString()
+
+    print("This is the undlerlings for $name: ")
+    println(tomteland.getUnderlings(name, list))
+}
